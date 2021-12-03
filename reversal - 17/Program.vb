168785251d6080db19457149afedec136f1aa8f5
Ollie Foster <00.aegis-aseries@icloud.com>
2021-12-03 22:15:14 +0000

    initial commit

---
+++ reversal - 17/Program.vb
@@ -0,0 +1,28 @@
+﻿'
+' Created by SharpDevelop.
+' User: 19ofoster
+' Date: 18/11/2021
+' Time: 13:49
+' 
+' To change this template use Tools | Options | Coding | Edit Standard Headers.
+'
+Module Program
+	Sub Main()
+		'Dimesions and array
+		Dim num(5) As Int64
+		
+		'Inputs data from the user into the array
+		For c=0 To 4
+			Console.WriteLine("Please enter a number . . . ")
+			num(c) = Console.ReadLine()
+		Next
+		
+		'Reads from the array in reverse order
+		For c=4 To 0 Step - 1
+			Console.WriteLine(num(c))
+		Next
+		
+		Console.Write("Press any key to continue . . . ")
+		Console.ReadKey(True)
+	End Sub
+End Module
